7b8ef621deacbff3c061220d8c2d3936a33064b8
Adithya Renduchintala <arenduchintala@Adithyas-MacBook-Pro.local>
2014-08-25 18:09:35 -0400

    word-align.sh

--- word-align.sh
+++ word-align.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/Cellar/moses/HEAD/scripts/training/train-model2.perl -root-dir ~/PycharmProjects/MacaronicTextGeneration/data/moses-files -external-bin-dir ~/source-pkgs/giza-pp/external-bin/ --corpus /Users/arenduchintala/PycharmProjects/MacaronicTextGeneration/data/training/train --f en --e de --first-step 1 --last-step 4
+/usr/local/Cellar/moses/HEAD/scripts/training/train-model2.perl -root-dir ~/PycharmProjects/MacaronicTextGeneration/data/moses-files -external-bin-dir ~/source-pkgs/giza-pp/external-bin/ --corpus /Users/arenduchintala/PycharmProjects/MacaronicTextGeneration/data/training/train.10 --f en --e de --first-step 1 --last-step 4
