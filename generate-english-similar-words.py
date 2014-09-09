84da722b89f31c1324a7dc9c935248e03c7d1a57
arendu <adithya.r@gmail.com>
2014-09-09 11:27:14 -0400

    gone back to 2 for loops

--- generate-english-similar-words.py
+++ generate-english-similar-words.py
@@ -18,14 +18,23 @@ if __name__ == '__main__':
     google_model = gensim.models.word2vec.Word2Vec.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
 
     for v in vocab_en:
-        try:
-            similar_v = google_model.most_similar(positive=[v], topn=20)
+        similar_v = []
+        for k in vocab_en:
+            try:
+                cosine_dist = google_model.similarity(v, k)
+                current_min = min(similar_v)[0]
+                if len(similar_v) <= 20:
+                    similar_v.append((cosine_dist, k))
+                elif cosine_dist > current_min:
+                    similar_v[similar_v.index(current_min)] = (cosine_dist, k)
+                else:
+                    pass
+            except KeyError:
+                pass
+        if len(similar_v) == 20:
             s = ' '.join(["{0:.4f}".format(s[1]) + "," + s[0] for s in similar_v])
-            print(v + '\t' + s)
-        except KeyError:
-            pass
-        except UnicodeEncodeError:
-            print 'unicode error', v
+            o = v + '\t' + ','.join([s[1] for s in similar_v])
+            print o.decode('utf-8')
 
 
 
