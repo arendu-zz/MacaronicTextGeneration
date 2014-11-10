ed54b7363d02ab1053fb76362a67842f4cc07c59
arendu <adithya.r@gmail.com>
2014-11-10 16:50:13 -0500

    modified word align to do truecase tokenize and clean

--- word-align.sh
+++ word-align.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-${MOSES_DIR}/scripts/training/train-model.perl -root-dir ${PROJECT_DIR}/data/moses-files -external-bin-dir ~/source-pkgs/giza-pp/external-bin/ --corpus ${PROJECT_DIR}/data/training/train.clean --f en --e de --first-step 1 --last-step 4
+#do tokenizing
+CORPUS_FOLDER="data/moses-files"
+CORPUS_PREFIX="train"
+${MOSES_DIR}/scripts/tokenizer/tokenizer.perl -l en < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.en > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.en
+${MOSES_DIR}/scripts/tokenizer/tokenizer.perl -l de < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.de > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.de
+
+#learn truecase model
+${MOSES_DIR}/scripts/recaser/train-truecaser.perl --model ${PROJECT_DIR}/${CORPUS_FOLDER}/truecase-model.en --corpus  ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.en
+${MOSES_DIR}/scripts/recaser/train-truecaser.perl --model ${PROJECT_DIR}/${CORPUS_FOLDER}/truecase-model.de --corpus  ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.de
+
+#do truecasing
+${MOSES_DIR}/scripts/recaser/truecase.perl --model ${PROJECT_DIR}/${CORPUS_FOLDER}/truecase-model.en < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.en > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.true.en
+${MOSES_DIR}/scripts/recaser/truecase.perl --model ${PROJECT_DIR}/${CORPUS_FOLDER}/truecase-model.de < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.de > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.true.de
+
+#do cleaning
+${MOSES_DIR}/scripts/training/clean-corpus-n.perl ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.true de en ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.clean.tok.true 1 80
+#${MOSES_DIR}/scripts/training/train-model.perl -root-dir ${PROJECT_DIR}/${CORPUS_FOLDER} -external-bin-dir ~/source-pkgs/giza-pp/external-bin/ --corpus ${PROJECT_DIR}/data/training/${CORPUS_PREFIX} --f en --e de --first-step 1 --last-step 4
