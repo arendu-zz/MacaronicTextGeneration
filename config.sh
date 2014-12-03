eee008a93ac03763848d6f38a69e71899a08b97b
arendu <adithya.r@gmail.com>
2014-12-03 14:26:12 -0500

    config file for moses training

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh 
+export PROJECT_DIR=/users/arenduc1/MachineTranslation/MacaronicTextGeneration
+export MOSES_DIR=/users/arenduc1/source-pkgs/mosesdecoder
+export EXTERNAL_BIN_DIR=/users/arenduc1/source-pkgs/giza-pp/external-bin/
+export CORPUS_FOLDER="data/moses-files" 
+export CORPUS_PREFIX="train"
