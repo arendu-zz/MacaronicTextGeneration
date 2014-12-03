42ac3ed2afbb1cbf1728cdc462ae6cb335bc38b0
arendu <adithya.r@gmail.com>
2014-12-03 14:32:16 -0500

    changed to word-align

--- word-align.sh
+++ word-align.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #do tokenizing
-CORPUS_FOLDER="data/europarl-full"
-CORPUS_PREFIX="europarl-v7"
 ${MOSES_DIR}/scripts/tokenizer/tokenizer.perl -l en < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.en > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.en
 ${MOSES_DIR}/scripts/tokenizer/tokenizer.perl -l de < ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.de > ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.de
 
@@ -17,4 +15,4 @@ ${MOSES_DIR}/scripts/recaser/truecase.perl --model ${PROJECT_DIR}/${CORPUS_FOLDE
 ${MOSES_DIR}/scripts/training/clean-corpus-n.perl ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.tok.true de en ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.clean.tok.true 1 80
 
 #do word align and phrase table
-${MOSES_DIR}/scripts/training/train-model.perl -root-dir ${PROJECT_DIR}/${CORPUS_FOLDER} -external-bin-dir ~/source-pkgs/giza-pp/external-bin/ --corpus ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.clean.tok.true --f en --e de --first-step 1 --last-step 6 
+${MOSES_DIR}/scripts/training/train-model.perl -root-dir ${PROJECT_DIR}/${CORPUS_FOLDER} -external-bin-dir ${EXTERNAL_BIN_DIR} --corpus ${PROJECT_DIR}/${CORPUS_FOLDER}/${CORPUS_PREFIX}.clean.tok.true --f de --e en --first-step 1 --last-step 6 --parallel 
