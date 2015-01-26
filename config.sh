4c8e7f200911c0e4dedcb104ce82ba23dab0f229
arendu <adithya.r@gmail.com>
2015-01-25 22:51:11 -0500

    changes to config

--- config.sh
+++ config.sh
@@ -2,5 +2,6 @@
 export PROJECT_DIR=/users/arenduc1/MachineTranslation/MacaronicTextGeneration
 export MOSES_DIR=/users/arenduc1/source-pkgs/mosesdecoder
 export EXTERNAL_BIN_DIR=/users/arenduc1/source-pkgs/giza-pp/external-bin/
+#export CORPUS_FOLDER="data/coursera-large" 
 export CORPUS_FOLDER="data/moses-files" 
 export CORPUS_PREFIX="train"
