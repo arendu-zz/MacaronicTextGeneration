546a4579b242951bfb17b462596efe20b605dae4
Adithya Renduchintala <arenduc1@b04.clsp.jhu.edu>
2015-04-17 19:28:04 -0400

    prep for translation makes spans as well

--- prepare-for-translations.sh
+++ prepare-for-translations.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-source /Users/arenduchintala/PycharmProjects/MacaronicTextGeneration/config.cfg
-#source /home/arenduc1/MachineTranslation/MacaronicTextGeneration/config.cfg
+#source /Users/arenduchintala/PycharmProjects/MacaronicTextGeneration/config.cfg
+source /home/arenduc1/MachineTranslation/MacaronicTextGeneration/config.cfg
 python make_substrings.py --ce $DATA_DIR/$FILE_PREFIX.en.20 --cd $DATA_DIR/$FILE_PREFIX.de.20
 #PARSER_OPTIONS="-binarize -confidence -kbest 10"
 PARSER_OPTIONS="-binarize "
-JAVA_OPTIONS="-XX:MaxPermSize=1024m -XX:MaxHeapSize=4048m"
-java  $JAVA_OPTIONS  -jar ~/source-pkgs/berkeleyparser/berkeleyParser.jar $PARSER_OPTIONS -gr ~/source-pkgs/berkeleyparser/ger_sm5.gr < $DATA_DIR/$FILE_PREFIX.de.20 > $DATA_DIR/$FILE_PREFIX.de.20.parse
+export JAVA_OPTIONS="-XX:MaxPermSize=1024m -XX:MaxHeapSize=4048m"
+java  $JAVA_OPTIONS  -jar $HOME/source-pkgs/berkeleyparser/berkeleyParser.jar $PARSER_OPTIONS -gr $HOME/source-pkgs/berkeleyparser/ger_sm5.gr < $DATA_DIR/$FILE_PREFIX.de.20 > $DATA_DIR/$FILE_PREFIX.de.20.parse
 #java  $JAVA_OPTIONS  -jar ~/source-pkgs/berkeleyparser/berkeleyParser.jar -gr ~/source-pkgs/berkeleyparser/ger_sm5.gr < $DATA_DIR/$FILE_PREFIX.de.20 > $DATA_DIR/$FILE_PREFIX.de.20.nonbin.parse
 python constituents.py -p $DATA_DIR/$FILE_PREFIX.de.20.parse
+python make_substrings.py --ce $DATA_DIR/$FILE_PREFIX.en.20 --cd $DATA_DIR/$FILE_PREFIX.de.20 #will write out .span and .txtspan
